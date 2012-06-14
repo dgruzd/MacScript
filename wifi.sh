c266cb5c107b1efa8bbb0a4bea2624ca0e0ce92e
Dmitry Gruzd <donotsendhere@gmail.com>
2012-06-14 15:32:54 +0400

    again

--- wifi.sh
+++ wifi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sudo curl http://goo.gl/xIOJQ -o /usr/bin/wifi && sudo chmod +x /usr/bin/wifi
+#sudo curl -L http://goo.gl/xIOJQ -o /usr/bin/wifi && sudo chmod +x /usr/bin/wifi
 device="$(networksetup -listallhardwareports |
 grep -E '(Wi-Fi|AirPort)' -A 1 | grep -o "en.")"
 [[ "$(networksetup -getairportpower $device)" == *On ]] && val=off || val=on
