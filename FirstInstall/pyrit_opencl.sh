6a1c11b7ee2ac8df928130ef22b69b3fff336cc7
Dmitry Gruzd <donotsendhere@gmail.com>
2016-11-17 11:09:03 +0300

    Fix opencl scapy

--- FirstInstall/pyrit_opencl.sh
+++ FirstInstall/pyrit_opencl.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Scapy 2.3.2
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-python/cddd216c719ea0c21cdf3d9ab9722ece6acbec3e/scapy.rb
+brew pin scapy
+
 cd /tmp/
 git clone https://github.com/0x90/pyrit.git
 cd pyrit/pyrit
